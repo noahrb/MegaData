7c3ac79700151577b72c81cc226257d609b48a7f
noahrb <nbev0918@740s-pl-127.local>
2017-02-27 12:17:13 -0700

    Updated List

--- MegaData/MegaData/Model/List.h
+++ MegaData/MegaData/Model/List.h
@@ -125,7 +125,48 @@ Type List<Type> :: getFromIndex(int index)
 template <class Type>
 Type List<Type> :: remove(int index)
 {
+        assert(index >= 0 && index < size);
+        Type removed;
+        
+        Node<Type> * current = front;
+        Node<Type> * previous = nullptr;
+        Node<Type> * toBeRemoved = nullptr;
+        
+        if(index == 0)
+        {
+            toBeRemoved = front;
+            this->front = front->getNodePointer();
+        }
+        else if(index == size - 1)
+        {
+            for (int spot = 0; spot < index; spot++)
+            {
+                previous = current;
+                current = current->getNodePointer();
+            }
+            
+            toBeRemoved = current;
+            previous->setNodePointer(nullptr);
+            this->end = previous;
+        }
+    else
+    {
+        for(int spot = 0; spot < index; spot++)
+        {
+            previous = current;
+            current = current->getNodePointer();
+        }
+        
+        toBeRemoved = current;
+        current = toBeRemoved->getNodePointer();
+        previous->setNodePointer(current);
+        
+    }
+    removed = toBeRemoved->getNodeData();
     
+    delete toBeRemoved;
+    size--;
+    return removed;
 }
 
 template <class Type>
