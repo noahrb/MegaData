152f633076f415e6e1cdba109fe6c6f83ddd8d67
Noah Beverly <noahbeverly@gmail.com>
2017-05-22 21:51:36 -0600

    Added Hash Table and Node classes!

---
+++ MegaData/MegaData/Model/HashNode.h
@@ -0,0 +1,58 @@
+//
+//  Node.h
+//  MegaData
+//
+//  Created by Beverly, Noah on 2/13/17.
+//  Copyright © 2017 Beverly, Noah. All rights reserved.
+//
+
+#ifndef HashNode_h
+#define HashNode_h
+
+template <class Type>
+class HashNode
+{
+private:
+    Type data;
+    long key;
+
+public:
+    HashNode();
+    HashNode(Type data);
+
+    Type getData();
+    long getKey() const;
+    void setData(Type data);
+};
+
+template <class Type>
+HashNode<Type> :: HashNode()
+{
+    this->key = rand();
+}
+
+template <class Type>
+HashNode<Type> :: HashNode(Type data)
+{
+    this->key = rand();
+    this->data = data;
+}
+
+template <class Type>
+long HashNode<Type> :: getKey() const
+{
+    return this->key;
+}
+
+template <class Type>
+Type HashNode<Type> :: getData()
+{
+    return this->data;
+}
+
+template <class Type>
+void HashNode<Type> :: setData(Type data)
+{
+    this->data = data;
+}
+#endif /* HashNode_h */
